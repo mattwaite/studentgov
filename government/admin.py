7e5f1c2abde6b46738a33bdc43508bed35d08446
Andrew McClure <andrewjmcclure1@gmail.com>
2011-03-30 16:27:37 -0500

    Fixed admin.py fields

--- government/admin.py
+++ government/admin.py
@@ -1,27 +1,16 @@
 from django.contrib import admin
 from studentgov.government.models import University, GovernmentName, Year, BodyType
-
-class UniversityAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'name_slug': ('name', ) }
-    search_fields = ['name']
-    pass
-
+    
 class GovernmentNameAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'name_slug': ('name', ) }
-    search_fields = ['name']
-    pass
-
-class Yeardmin(admin.ModelAdmin):
-    prepopulated_fields = {'name_slug': ('name', ) }
-    search_fields = ['name']
-    pass
-
+    prepopulated_fields = {'slug': ('name',) }
+    
+class YearAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('year',) }
+    
 class BodyTypeAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'name_slug': ('name', ) }
-    search_fields = ['name']
-    pass
+    prepopulated_fields = {'slug': ('type',) }
 
-admin.site.register(University, UniversityAdmin)
+admin.site.register(University)
 admin.site.register(GovernmentName, GovernmentNameAdmin)
 admin.site.register(Year, YearAdmin)
 admin.site.register(BodyType, BodyTypeAdmin)
