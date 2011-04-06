c7eaeb9c8069494565ead22d9046ff6b162f0c3d
Marcus Scheer <marcus.scheer@gmail.com>
2011-04-06 17:12:22 -0500

    updated the people urls and views

--- people/urls.py
+++ people/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls.defaults import *
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', '.people.views.homepage'),
-    (r'^$', 'people.views.body_type'),
-    (r'^people/body_type(?P<body_type>[-\w]+)/$', 'people.views.body'),
-    (r'^people/body_type/body/$', 'people.views.person'),
+    (r'^(?P<year>[-\w]+)/$', '.people.views.year'),
+    (r'^(?P<year>[-\w]+)/(?P<body>[-\w]+)/$', 'people.views.body_type'),
+    (r'^(?P<year>[-\w]+)/(?P<body>[-\w]+)/$', 'people.views.body'),
+    (r'^(?P<year>[-\w]+)/(?P<body>[-\w]+)/$$', 'people.views.person'),
     (r'^admin/', include(admin.site.urls)),
 
 )
--- people/views.py
+++ people/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from people.models import Person, BodyYear
+from government.models import BodyType, Year
+from django.shortcuts import render_to_response
+
+
+
+def year(request, year):
+    govtyear = BodyYear.objects.filter(year=year)
+    return render_to_response('people/year.html', {
+        'govtyear': govtyear,
+    })
+
