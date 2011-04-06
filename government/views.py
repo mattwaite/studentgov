6ac1ffa5225c0454dd39611858eae3439f23d7a3
Andrew McClure <andrewjmcclure1@gmail.com>
2011-04-06 17:10:32 -0500

    Fiddling.

--- government/views.py
+++ government/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from government.models import University, GovernmentName, Year, BodyType, Body
 from people.models import BodyYear
@@ -11,11 +12,11 @@ def year(request, year):
         'year': year,
 })
 
-def body(request, body):
-    return HttpResponse(body)
-    
-def name(request, body, name):
-    return HttpResponse(name)
-    
-def standing(request):
-    return HttpResponse("Senior.")
+#def body(request, year, body):
+#    return HttpResponse(body)
+#    
+#def name(request, year, body, name):
+#    return HttpResponse(name)
+#    
+#def standing(request):
+#    return HttpResponse("Senior.")
