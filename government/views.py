7a52d0838fecaf7f0ea63398bf49165db7cc0d22
Andrew McClure <andrewjmcclure1@gmail.com>
2011-04-06 16:36:09 -0500

    Updated government/views.py file

--- government/views.py
+++ government/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+def homepage(request):
+    return HttpResponse("Welcome to the ASUN Tracker.")
+    
+def year(request):
+    return HttpResponse("NVision 2010-2011.")
+    
+def body(request):
+    return HttpResponse("Senate.")
+    
+def name(request):
+    return HttpResponse("Andrew McClure.")
+    
+def standing(request):
+    return HttpResponse("Senior.")
